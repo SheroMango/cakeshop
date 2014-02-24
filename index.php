4197370960fa5ff216d718117fe4fd761c156a92
ChanMo <orangechen.1991@gmail.com>
2014-02-24 18:16:51 +0800

    index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * 入口文件
+ * @version 2013-01-25
+ */
+//定义项目名称和路径
+define('APP_NAME', 'app');
+define('APP_PATH', './app/');
+define('APP_DEBUG', true);
+// 加载框架入口文件
+require( './core/ThinkPHP/ThinkPHP.php');
